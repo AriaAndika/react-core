849ae32b5c6d19221058918703aa3a367ac055b9
Aria Andika <andikaakun17@gmail.com>
2022-09-12 16:43:50 +0700

    Project init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='panel'>
+      
+      <input type={'text'} placeholder={'judul..'} className={'panelInput'}></input>
+      <input type={'number'} placeholder={'deadline..'} className={'panelInput'}></input>
+      <textarea cols={25} rows={10} className={'panelInput'}></textarea>
     </div>
   );
 }
